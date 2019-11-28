f3d9b12aa216f724ea94c650d9c1e46aee6e3fab
Amy Balmforth <amy.balmforth@gmail.com>
2019-11-28 13:04:37 +0000

    last commit before deleting code and writing tests

--- note-controller.js
+++ note-controller.js
@@ -15,24 +15,10 @@
     element.innerHTML = myview;
   }
 
-
+  var noteview = new Noteview(notelist = new Notelist);
+  var controller = new Notecontroller(notelist = new Notelist);
+  //controller.addHTML();
 
 
   exports.Notecontroller = Notecontroller;
 })(this);
-
-// var noteview = new Noteview(notelist = new Notelist);
-//var controller = new Notecontroller(notelist = new Notelist);
-
-
-//var element = document.getElementById("app");
-//console.log(element);
-//element.innerHTML = "howdy!";
-
-// Code uses the constructor and prototype pattern to define a note controller that can be instantiated.
-
-// The note controller constructor does the following:
-// Takes a note list model as a parameter.
-// Adds a note that says Favourite drink: seltzer.
-// Creates a note list view, passing in the note list model.
-// The note controller has a method that gets HTML from the note list view and inserts it into the app element.
